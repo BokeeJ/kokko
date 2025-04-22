d5437b0c88f97000888ff366d9023116763c6b34
BokeeJ <bokeejusthard@gmail.com>
2025-04-22 23:44:57 +0200

    started navBar

--- src/ components/Navbar.jsx
+++ src/ components/Navbar.jsx
@@ -3,29 +3,57 @@ import React from 'react'
 import { CiInstagram } from "react-icons/ci";
 import { CiFacebook } from "react-icons/ci";
 
+
 function Navbar() {
     return (
         <div>
-            {/* header */}
-            <div className='flex bg-pink-100 justify-between'>
-                <div className='flex'>
-                    <CiInstagram className='cursor-pointer m-2' size={40} />
-                    <CiFacebook className='cursor-pointer m-2' size={40} />
+            <div className='lg:hidden'>
+                <div className='flex justify-around items-center w-full h-[80px] bg-white shadow-md px-5'>
+                    {/* left side */}
+                    <div>
+                        burger
+                    </div>
+                    <div>
+                        <img src="/logo.svg" alt="logoKokko" className='w-[150px] h-[50px] object-cover bg-transparent' />
+                    </div>
+                    <div className='flex gap-2'>
+                        <CiInstagram className='text-3xl cursor-pointer hover:text-pink-400' />
+                        <CiFacebook className='text-3xl cursor-pointer hover:text-pink-400' />
+                    </div>
+
+
+
                 </div>
-                <div className='flex items-center gap-2'>
-                    <img src="/english2.jpg" className='w-7 h-7' alt="logoENG" />
-                    <h2 className=' text-2xl'>|</h2>
-                    <img src="/serb.jpg" className='h-7 w-7' alt="logoSRP" />
-                    {/* slika logoa */}
+            </div>
+
+            < div className='flex w-full h-[200px] ' >
+                {/* logo */}
+                <div className='w-[500px] h-[200px] px-4 flex items-center'>
+                    <img src="/logo.svg" alt="logoKokko" className='w-[400px] h-full object-contain bg-transparent' />
                 </div>
 
-            </div>
-            {/* nav */}
-            <div className=''>
-                <img src="/logo.svg" alt="logoKokko" className='' />
-            </div>
+                {/* navLinks */}
+                <div className='flex justify-center items-center w-[80%]' >
+                    <ul className="flex justify-center gap-10 lg:text-3xl text-xl font-light">
+                        <li className="cursor-pointer hover:text-pink-400 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-pink-400 after:w-0 hover:after:w-full after:transition-all after:duration-300">
+                            Naslovna
+                        </li>
+                        <li className="cursor-pointer hover:text-pink-400 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-pink-400 after:w-0 hover:after:w-full after:transition-all after:duration-300">
+                            Katalog
+                        </li>
+                        <li className="cursor-pointer hover:text-pink-400 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-pink-400 after:w-0 hover:after:w-full after:transition-all after:duration-300">
+                            Kontakt
+                        </li>
+                        <li className="cursor-pointer hover:text-pink-400 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-pink-400 after:w-0 hover:after:w-full after:transition-all after:duration-300">
+                            Galerija
+                        </li>
+                    </ul>
 
-        </div>
+                </div>
+            </div >
+
+
+        </div >
     )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Navbar from "./ components/navbar"
-
+import Main from "./ components/Main"
 function App() {
 
 
   return (
     <div>
       <Navbar />
-      <div className="h-screen flex justify-center items-center">
-        <h1 className="text-5xl text-amber-400">cao Katrin, cao ko<span className="text-6xl text-pink-400">kk</span>o</h1>
-      </div>
+      <Main />
     </div>
 
   )
