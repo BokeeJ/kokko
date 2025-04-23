7cfbb6090c8f83ac74b6abdb9d7cf09683298e76
BokeeJ <bokeejusthard@gmail.com>
2025-04-23 23:35:45 +0200

    fixed vercel/tailwind

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -27,14 +27,14 @@ function Navbar() {
                 </div>
             </div>
 
-            < div className='flex w-full h-[200px] justify-center lg:block md:block hidden' >
+            < div className='lg:flex md:flex w-full h-[200px] justify-center  hidden' >
                 {/* logo */}
-                <div className='w-[500px] h-[200px] px-4 flex items-center hidden lg:block '>
+                <div className='w-[500px] h-[200px] px-4  items-center hidden lg:flex '>
                     <img src="/logo.svg" alt="logoKokko" className='w-[400px] h-full object-cover bg-transparent' />
                 </div>
 
                 {/* navLinks */}
-                <div className='flex justify-center items-center w-[80%] hidden' >
+                <div className='lg:flex justify-center items-center w-[80%] hidden' >
                     <ul className="flex justify-center lg:gap-5 gap-2 lg:text-3xl text-l font-light">
                         <li className="cursor-pointer hover:text-pink-400 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-pink-400 after:w-0 hover:after:w-full after:transition-all after:duration-300">
                             Naslovna
