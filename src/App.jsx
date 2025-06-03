8cd55cf94b41ea1d619ef6f1ad643f36f045b43e
BokeeJ <bokeejusthard@gmail.com>
2025-06-04 01:02:01 +0200

    test10

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import FooterEnd from "./Components/FooterEnd";
+
+
 
 function App() {
   return (
     <div className="relative min-h-screen bg-black overflow-x-hidden">
+      <Header />
       <Navbar />
       <div className="pt-[100px] lg:pt-0">
         <Outlet />
       </div>
+      <Footer />
+      <FooterEnd />
     </div>
   );
 }
